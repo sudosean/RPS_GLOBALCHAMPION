ec1770decdd34ef79aabc2771a943e01106ad147
johnelliott <johnelliott703@gmail.com>
2014-11-17 18:02:46 -0500

    add update method to rounds controller and add status method to round model

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -4,8 +4,19 @@ class RoundsController < ApplicationController
   end
 
 
-  def update
-    # smart params
-    # this should be a standard update
+def update
+    round = Round.find(params[:id])
+    round.update!(round_params)
+    redirect_to round
   end
+
+  private
+    # Using a private method to encapsulate the permissible parameters
+    # is just a good pattern since you'll be able to reuse the same
+    # permit list between create and update. Also, you can specialize
+    # this method with per-user checking of permissible attributes.
+    def round_params
+      params.require(:round).permit(:player_1_hand, :player_2_hand)
+      # :player_1_id, :player_2_id, :player_1_hand, :player_2_hand could be the new allowed thing
+    end
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -4,4 +4,10 @@ class Round < ActiveRecord::Base
   def player_number(current_user)
     '1'
   end
+
+  def status
+    "waiting"
+    # now this is hard-coded, but we have to make it consider whether we're waiting, meaning do we have both platers
+    # do a unit test for what waiting and not waiting looks like
+  end
 end
