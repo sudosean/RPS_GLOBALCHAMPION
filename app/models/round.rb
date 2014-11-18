a9acc69ebeb3e46a62cba9eaa5f2e45730a4529c
seanrlarge <sean.large1@gmail.com>
2014-11-18 16:29:28 -0500

    multiplayer shows status of when player hands are thrown

--- app/models/round.rb
+++ app/models/round.rb
@@ -3,13 +3,16 @@ class Round < ActiveRecord::Base
 
   def player_number(current_user)
     1
+
   end
 
   # Do we have both players answers locked in?
   # do a unit test for what waiting and not waiting looks like
   def status
-    if player_1_hand == nil || player_2_hand == nil
-      "waiting"
+   if player_1_hand == nil && player_2_hand == nil
+      return "input"
+    elsif player_1_hand == nil || player_2_hand == nil
+      return "waiting"
     else
       "complete"
     end
--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,23 +1,35 @@
 require 'rails_helper'
 
 describe "Round" do
+  subject(:round) { Round.new }
+
   describe "when neither player has selected a hand" do
-    round = Round.new
-    it "should return waiting" do
-      expect(round.status).to eq('waiting')
+    it "should return input" do
+      expect(round.player_1_hand).to be_nil
+      expect(round.player_2_hand).to be_nil
+      expect(round.status).to eq('input')
     end
   end # end of describe
-  describe "When both players choose a hand" do
-    round = Round.new
-    round.update(player_1_hand: "rock", player_2_hand: "scissors")
+
+  context "When both players choose a hand" do
+    subject(:round) do
+      Round.new(player_1_hand: "rock", player_2_hand: "scissors")
+    end
+
     it "should return complete" do
+      expect(round.player_1_hand).to_not be_nil
+      expect(round.player_2_hand).to_not be_nil
       expect(round.status).to eq('complete')
     end
   end # end of second describe
+
   describe "When only one player has a hand played" do
-    round = Round.new
-    round.update(player_2_hand: "scissors")
+    subject(:round) do
+      Round.new(player_2_hand: "scissors")
+    end
     it "should return waiting" do
+      expect(round.player_1_hand).to be_nil
+      expect(round.player_2_hand).to_not be_nil
       expect(round.status).to eq('waiting')
     end
   end # end of third describe
