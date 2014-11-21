1301eebaadd03e91961f70384177022455ad13e2
seanrlarge <sean.large1@gmail.com>
2014-11-21 14:48:00 -0500

    refactor turnary method that looks to see if player 1 is current_user

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,9 +4,13 @@ class Round < ActiveRecord::Base
   belongs_to :player_2, class_name: 'User'
 
   scope :open, -> { where(player_2_id: nil) }
-
+# refactor this
   def player_number(current_user)
-    (player_1 == current_user) ? 1 : 2
+    if (player_1 == current_user)
+      return 1
+      else
+        return 2
+    end
   end
 
   # Do we have both players answers locked in?
